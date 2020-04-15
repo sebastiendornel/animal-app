00e4844bb93c554121a5daa6fa635bfa545b9528
kaidornel <kai_dornel@hotmail.com>
2020-04-15 11:51:37 -0400

    full user CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,39 @@ class UsersController < ApplicationController
     # styling
     # specs
     # analytics page
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        redirect_to user_path(@user)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to shelters_path
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :age, :address, :pet_qty, :bio)
+    end
 end
